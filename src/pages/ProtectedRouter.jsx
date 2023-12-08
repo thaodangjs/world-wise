896cada4e2a2c87945baccf4497043f845379d43
Thao Dang <thaodang.js@gmail.com>
2023-12-08 10:32:28 +0700

    Protect a Route

---
+++ src/pages/ProtectedRouter.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
+
+function ProtectedRouter({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate("/");
+    },
+    [isAuthenticated, navigate]
+  );
+
+  return isAuthenticated ? children : null;
+}
+
+export default ProtectedRouter;
